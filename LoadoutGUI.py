3e21a18cf0d61907d48a39faaba0e2b2bc2a0338
Builder-roberts <99707221+Builder-roberts@users.noreply.github.com>
2022-02-22 14:28:22 -0700

    Add files via upload
    2.6 - fixed the itemspawn bug, added research regression

--- LoadoutGUI.py
+++ LoadoutGUI.py
@@ -4,9 +4,11 @@ from tkinter import Menu, ttk
 from tkinter import filedialog
 import getpass
 import re
-import os.path
+import os
 from tkinter import messagebox
 
+
+uwu = 0
 username = getpass.getuser()
 filename = "C:/Users/"+username+"/AppData/Local/Synthetik/Save.sav"
 tfilename = "C:/Users/"+username+"/AppData/Local/Synthetik/TEMPORARYSAVE.txt"
@@ -20,7 +22,10 @@ def browsefiles():
     print(filepath[0])
 
 if not os.path.exists(filename):
-    messagebox.showinfo('Synfwetik','Sowwy UwU, yowr swave fiwe is nwot whewe I toht it wood bwe! OwO! could you pwease fwind it fwwor me UwU?')
+    if uwu == 0:
+        messagebox.showinfo('Synthetik','Sorry, your save file is not where I thought it would be! Could you please find it for me?')
+    elif uwu == 1:
+        messagebox.showinfo('Synfwetik','Sowwy UwU, yowr swave fiwe is nwot whewe I toht it wood bwe! OwO! could you pwease fwind it fwwor me UwU?')
     browsefiles()
 
 root = tk.Tk()
@@ -214,13 +219,14 @@ def CopytoSave():
 
 def AutoModuleEdit(power):
     if re.search("(-?(0|[1-9]\d*)?(\.\d+)?(?<=\d)(e-?(0|[1-9]\d*))?|0x[0-9a-f]+)",power) == None:
-        messagebox.showinfo('UwO','Sowwy UwU, yow pwessed the AutoModule buttwon wifouf pwutwing a gwood input in! Try uswing only numbwars dis twime, :3 ohkway?')
+        if uwu == 0:
+            messagebox.showinfo('Power error','Sorry, you pressed the AutoModule button without putting a good input in! Try using only numbers this time, okay?')
+        elif uwu == 1:
+            messagebox.showinfo('UwO','Sowwy UwU, yow pwessed the AutoModule buttwon wifouf pwutwing a gwood input in! Try uswing only numbwars dis twime, :3 ohkway?')
         return
     with open(filename, "r") as Save, open(tfilename, "w") as Tsave:
         TruePower = '"' + power + '"'
         for line in Save:
-            #write content to new file
-            #Just find the tpoints_perk thing and edit every line that starts with that. This only works for perks & cores, specifically the Power of said.
             if line.startswith("tpoints_obj_perk_"):
                 Tsave.write(re.sub('"-?\d+(\.\d+)?"', TruePower, line))
                 continue
@@ -229,12 +235,10 @@ def AutoModuleEdit(power):
 
 def OPModuleEdit():
     OPpower=["10.000000","2.500000","2.000000","3.000000","-7.000000","7.000000","30.000000","50.000000","10.000000","15.000000","6.000000","4.000000","4.000000","5.000000","10.000000","3.000000","1.000000","1.000000","8.000000","7.000000","5.000000","10.000000","7.000000","2.500000","40.000000","7.000000","5.000000","20.000000","-0.000100","10.000000","7.000000","7.000000","3.000000","1","1","1","1","15.000000","10.000000","8.000000","4.000000","5.500000","2.900000","1.600000","10.000000","7.000000","5.000000","6.000000","20.000000","20.000000","2.500000","10.000000","1","1","10.000000","20.000000","20.000000","30.000000","20.00000","20.000000","4.000000","5.000000","10.000000","2.500000","10.000000","2.00000","7.000000","7.000000","-34.000000","12.000000","1","1","1"]
-    print("Overpowered module power editing (your mileage may vary)")
+    messagebox.showinfo("Ymmv","Overpowered module power editing (your mileage may vary)")
     with open(filename, "r") as Save, open(tfilename, "w") as Tsave:
         j = 0
         for line in Save:
-            #write content to new file
-            #Just find the tpoints_perk thing and edit every line that starts with that. This only works for perks & cores, specifically the Power of said
             if line.startswith("tpoints_obj_perk_"):
                 item = OPpower[j]
                 ActiveLine = '"' + item + '"'
@@ -276,22 +280,26 @@ def AutoWeaponSpawnEdit():
                 wepID = wepID.group()
                 weplist.append(wepID[1:-1])
         if len(weplist) == 0:
-            #put an info here
-            messagebox.showinfo("Towken Ewwow","Sowwy UwU, wwould you gow awdd a powwa towken two the weapon you want?")
+            if uwu == 0:
+                messagebox.showinfo("Token Error","Sorry, would you go add a power token to the weapon(s) you want?")
+            elif uwu == 1:
+                messagebox.showinfo("Towken Ewwow","Sowwy UwU, wwould you gow awdd a powwa towken two the weapon(s) you want?")
             return
     with open(filename, "r") as Save, open(tfilename, "w") as Tsave:
         if len(weplist) != 0:
-            #convert this to an info
-            messagebox.showinfo("Just so you know","Setting Drop chances for tokened weapons to 10000, and all others to -100.\n This only works for one run, one time per weapon pickup.\n(this does not stop weapon shops from spawning with 4 other weapons)")
+            if uwu == 0:
+                messagebox.showinfo("Just so you know","Setting Drop chances for tokened weapons to 10000, and all others to -100.\n This only works for one run, one time per weapon pickup.\n(this does not stop weapon shops from spawning with 4 other weapons)")
+            elif uwu == 1:
+                messagebox.showinfo("just so uwu know","setting dwop chances fow tokened weapons tuwu 10000, awnd aww othews tuwu -100.\n thiws onwy wowks fow owne wun, owne time pew weapon pickup.\n(this does nowt stowp weapon shops fwom spawning with 4 othew weapons)")
             for line in Save:
                 if line.startswith("wdropchange"):
                     for weapon in weplist:
-                        if line.startswith("wdropchange"+weapon):
+                        if line.startswith("wdropchange"+weapon+"="):
                             Tsave.write(re.sub('"-?\d+\.\d+"','"10000.000000"',line))
                             break
                         else:
                             continue
-                    if not(line.startswith("wdropchange"+weapon)):
+                    if not(line.startswith("wdropchange"+weapon+"=")):
                         Tsave.write(re.sub('"-?\d+\.\d+"','"-100.000000"',line))
                 else:
                     Tsave.write(line)
@@ -309,20 +317,26 @@ def AutoItemSpawnEdit():
                 itemID = itemID.group()
                 itemlist.append(itemID[1:-1])
         if len(itemlist) == 0:
-            messagebox.showinfo("Towken Ewwow","Sowwy UwU, wwould you gow awdd a powwa towken two te itwem you want?")
+            if uwu == 0:
+                messagebox.showinfo("Towken Error","Sorry, would you go add a power token to the item you want?")
+            elif uwu == 1:
+                messagebox.showinfo("Towken Ewwow","Sowwy UwU, wwould you gow awdd a powwa towken two te itwem you want?")
             return
     with open(filename, "r") as Save, open(tfilename, "w") as Tsave:
         if len(itemlist) != 0:
-            messagebox.showinfo("Just so you know","Setting Drop chances for tokened items to 100. Setting drop chances for all other items to -100.\nThis only works for one run, one time per item. OTHER ITEMS WILL NOT SPAWN\nThis might also be a bit off kilter- perhaps the tokened item ids do not corellate 1-1 with thier spawn ids")
+            if uwu == 0:
+                messagebox.showinfo("Just so you know","Setting Drop chances for tokened items to 100. Setting drop chances for all other items to -100.\nThis only works for one run, one time per item. OTHER ITEMS WILL NOT SPAWN\nThis might also be a bit off kilter- perhaps the tokened item ids do not corellate 1-1 with thier spawn ids")
+            elif uwu == 1:
+                messagebox.showinfo("just so uwu know","setting dwop chances fow tokened items tuwu 100. Setting dwop chances fow aww othew items tuwu -100.\nthis onwy wowks fow owne wun, owne time pew item. Othew items wiww nowt spawn\nthis might awso be a bit off kiwtew- pewhaps the tokened item ids duwu nowt cowewwate 1-1 with thiew spawn ids")
             for line in Save:
                 if line.startswith("idropchange"):
                     for item in itemlist:
-                        if line.startswith("idropchange "+item):
+                        if line.startswith("idropchange "+item+"="):
                             Tsave.write(re.sub('"-?\d+\.\d+"','"10000.000000"',line))
                             break
                         else:
                             continue                        
-                    if not(line.startswith("idropchange "+item)):
+                    if not(line.startswith("idropchange "+item+"=")):
                         Tsave.write(re.sub('"-?\d+\.\d+"','"-100.000000"',line))
                 else:
                     Tsave.write(line)
@@ -346,6 +360,46 @@ def itemspawnreset():
             else:
                 Tsave.write(line)
     Safetywindow()
+
+def undoresearch():
+    with open(filename,"r") as Save, open(tfilename,"w") as Tsave:
+        for line in Save:
+            if line.startswith("resunlock"):
+                Tsave.write(re.sub('"-?\d+\.\d+"','"0.000000"',line))
+            else:
+                Tsave.write(line)
+    Safetywindow()
+
+def openSave():
+    os.startfile("C:/Users/"+username+"/AppData/Local/Synthetik/save.sav")
+
+def UwU():
+    global uwu
+    if uwu == 0:
+        Class10.configure(text="Wiot Guawd")
+        Class11.configure(text="Bweachew")
+        Class20.configure(text="Sniwpew")
+        Class21.configure(text="Asswsasswin")
+        Class30.configure(text="Waidew")
+        Class31.configure(text="Heavwy Gunnew")
+        Class40.configure(text="Engineew")
+        Class41.configure(text="Demolishew")
+        Submit .configure(text="submwit")
+        Auto.configure(text="Auto Mwoduwle Edwit")
+        uwu = 1
+    elif uwu == 1:
+        Class10.configure(text="Riot Guard")
+        Class11.configure(text="Breacher")
+        Class20.configure(text="Sniper")
+        Class21.configure(text="Assassin")
+        Class30.configure(text="Raider")
+        Class31.configure(text="Heavy Gunner")
+        Class40.configure(text="Engineer")
+        Class41.configure(text="Demolisher")
+        Submit .configure(text="submit")
+        Auto.configure(text="Auto Module Edit")
+        uwu = 0
+        
 
 Class10 = tk.Button(root, text="Riot Guard",fg="white", bg="blue",command=lambda: Testfunc("10"))
 Class10.grid(row=0,column=0)
@@ -371,17 +425,24 @@ Autopower = tk.Entry(root)
 Autopower.grid(row=8,column=2)
 
 def about():
-    messagebox.showinfo('Synthetik Python Mod','By: Builder_Roberts\nMade for Synthetik 1!')
+    if uwu == 0:
+        messagebox.showinfo('Synthetik Python Mod','By: Builder_Roberts\nMade for Synthetik 1!')
+    elif uwu == 1:
+        messagebox.showinfo('synthetik python mod','by: Buiwdew_Wobewts\nmade fow Synthetik 1!')
 
 def notworking():
-    messagebox.showinfo('Actual help','first: try opening and closing synthetik. Make sure Synthetik is closed. That will reset the save file to what Synthetik Needs.\nSecond: message @Mason on the discord. He is the creator of this after all.')
-
+    if uwu == 0:
+        messagebox.showinfo('Actual help','first: try opening and closing synthetik. Make sure Synthetik is closed. That will reset the save file to what Synthetik Needs.\nSecond: message @Mason on the discord. He is the creator of this after all.')
+    elif uwu == 1:
+        messagebox.showinfo('actuaw hewp','fiwst: twy opening awnd cwosing synthetik. Make suwe synthetik iws cwosed. Thawt wiww weset the save fiwe tuwu whawt synthetik needs.\nsecond: message @mason own the discowd. He iws the cweatow of thiws aftew aww.')
 
 MenuBar = Menu(root)
 file = Menu(MenuBar,tearoff=0)
 file.add_command(label="Open",command=browsefiles)
+file.add_command(label="Show",command=openSave)
 file.add_command(label="Exit",command=root.quit)
 MenuBar.add_cascade(label="File",menu=file)
+
 power = Menu(MenuBar,tearoff=0)
 power.add_command(label="OPauto",command=OPModuleEdit)
 power.add_command(label="Quick 1.6x",command=lambda: AutoModuleEdit("1.60000"))
@@ -390,6 +451,8 @@ MenuBar.add_cascade(label="Power",menu=power)
 misc = Menu(MenuBar,tearoff=0)
 misc.add_command(label="Daily Run Reset",command=NewDailyRun)
 misc.add_command(label="Max Data",command=MaxData)
+misc.add_command(label="Undo Research",command=undoresearch)
+misc.add_command(label="UwU OwO",command=UwU)
 MenuBar.add_cascade(label="Misc",menu=misc)
 
 spawn = Menu(MenuBar,tearoff=0)
